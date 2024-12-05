02b851380c9b1124d838769b426e6e7f8414f6fe
CaNaZa <jonascanaza@gmail.com>
2024-12-05 19:23:30 -0300

    Se agrega el boton de whatsapp y se empieza a cambiar el diseño de la pagina

--- js/index_funtions.js
+++ js/index_funtions.js
@@ -1,1 +1,14 @@
-// FUNCIONES
+
+
+// FUNCION PARA USAR WHATSAPP
+function sendWhatsAppMessage() {
+    const phoneNumber = "541123662546";
+
+    const message = "Hola, estoy interesado en tus servicios. ¿Podríamos hablar?";
+
+    const encodedMessage = encodeURIComponent(message);
+
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappUrl, '_blank');
+}
