c85ab8586800e35800e67cd8dc22dcc31096e772
CaNaZa <jonascanaza@gmail.com>
2024-12-05 23:13:01 -0300

    bugfix v1.0

--- js/index_funtions.js
+++ js/index_funtions.js
@@ -1,13 +1,29 @@
+// FUNCIONES CERRAR MENU
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        
+        if (navbarCollapse.classList.contains('show')) {
+            new bootstrap.Collapse(navbarCollapse).hide();
+        }
+    });
+});
 
+document.addEventListener('click', function (event) {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    const isClickInsideNav = event.target.closest('.navbar');
+    const isNavOpen = navbarCollapse.classList.contains('show');
+
+    if (!isClickInsideNav && isNavOpen) {
+        new bootstrap.Collapse(navbarCollapse).hide();
+    }
+});
 
 // FUNCION PARA USAR WHATSAPP
 function sendWhatsAppMessage() {
     const phoneNumber = "#";
-
     const message = "Hola, estoy interesado en tus servicios. ¿Podríamos hablar?";
-
     const encodedMessage = encodeURIComponent(message);
-
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
     window.open(whatsappUrl, '_blank');
